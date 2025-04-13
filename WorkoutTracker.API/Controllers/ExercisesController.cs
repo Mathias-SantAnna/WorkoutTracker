0ba8f60ffd34cf2da71e56c375b781bbcbfdbf54
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2025-04-13 14:37:23 +0100

    Simplify API implementation to meet project requirements and update Readme file

---
+++ WorkoutTracker.API/Controllers/ExercisesController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkoutTracker.API.Data;
+using WorkoutTracker.API.Models;
+
+namespace WorkoutTracker.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExercisesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/exercises
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Exercise>>> GetExercises()
+        {
+            return await _context.Exercises.ToListAsync();
+        }
+
+        // GET: api/exercises/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exercise>> GetExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return exercise;
+        }
+    }
+}
--- WorkoutTracker.Web/obj/Debug/net8.0/WorkoutTracker.Web.AssemblyInfo.cs
+++ WorkoutTracker.Web/obj/Debug/net8.0/WorkoutTracker.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutTracker.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960a78be0c3c817164339c50b6f66e63eb21fc78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe297ca2e92c5a3700bc234c9c8d901eaf00666")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutTracker.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutTracker.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
