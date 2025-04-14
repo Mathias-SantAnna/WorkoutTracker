f07dd03e766d5a127a8c5a334011a2b77d4826a7
Pedro Wenceslau <58569347+wenceslaupedro@users.noreply.github.com>
2025-04-14 19:45:03 +0100

    Update Program.cs

--- WorkoutTracker.API/Program.cs
+++ WorkoutTracker.API/Program.cs
@@ -1,14 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
